9ef5a17441b05813c3cfdf569517a3926d07772d
adaraiseh <adaraiseh@outlook.com>
2023-12-16 17:17:22 -0500

    delete unneeded running store file

--- src/plugin/running/store.h
+++
@@ -1,8 +0,0 @@
-#ifndef ONM_TC_PLUGIN_RUNNING_STORE_H
-#define ONM_TC_PLUGIN_RUNNING_STORE_H
-
-#include "plugin/context.h"
-
-int onm_tc_running_store(onm_tc_ctx_t *ctx, sr_session_ctx_t *session);
-
-#endif // ONM_TC_PLUGIN_RUNNING_STORE_H
